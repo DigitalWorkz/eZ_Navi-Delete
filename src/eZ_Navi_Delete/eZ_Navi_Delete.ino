28b8552d99c549af1f239ef9f6d7f6cca33cbca6
radensb <35448905+radensb@users.noreply.github.com>
2021-04-30 02:00:01 -0700

    Added more bringup images

--- src/eZ_Navi_Delete/eZ_Navi_Delete.ino
+++ src/eZ_Navi_Delete/eZ_Navi_Delete.ino
@@ -8,15 +8,6 @@ Nissan370Z_AC_System* AC_System;
 
 void setup()
 {
-    //disable unused ports
-    pinMode(PA8, INPUT);  //BT Config
-    pinMode(PA9, INPUT);  //UART1 Tx
-    pinMode(PA10, INPUT); //UART1 Rx
-    pinMode(PB10, INPUT); //SCL2
-    pinMode(PB11, INPUT); //SDA2
-    pinMode(PB15, OUTPUT_OPEN_DRAIN); //BT Reset
-    digitalWrite(PB15, LOW); //hold BT in reset
-    
     pinMode(CAN_INT_PIN, INPUT);
     mcp_can1 = new MCP2515(CAN_CS_PIN);
     AC_System = new Nissan370Z_AC_System(&Serial2, mcp_can1);
@@ -35,10 +26,5 @@ void loop()
     AC_System->sendDialResponse();
     AC_System->updateCAN();
 
-    ///Fix me!:
-    //Periodically check the INT pin isnt stuck due to falling edge during SPI transfer
-    //This shouldnt be needed, but sometimes a falling is missed which results in a
-    //situation where the ISR never gets called again. This ensures it will but is not
-    //desired operation.
     if(!digitalRead(CAN_INT_PIN)) AC_System->can_isr();
 }
